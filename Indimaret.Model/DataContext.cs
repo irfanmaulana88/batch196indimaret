bd3a5e40d3776a96938e00701d1fd6bf6e613543
irfanmaulana88 <irfanmaulanaa8888@gmail.com>
2019-05-28 10:00:21 +0700

    first commit

---
+++ Indimaret.Model/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;//
+using System.Data.Entity.ModelConfiguration.Conventions;//
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indimaret.Model
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("Name=IndimaretConn")
+        {
+            //Database.SetInitializer(new Initializer());
+            Database.SetInitializer<DataContext>(null);
+        }
+
+        public virtual DbSet<Barang> Barang { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
